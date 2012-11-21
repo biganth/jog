4cf09c1b91d1aa2bf8968dd2b2211dd4fe2af4f0
Anthony DeFreitas <anthony@caribbeanhotels.com>
2012-11-21 11:41:04 -0400

    Back to original jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,16 +28,3 @@
 //= require bootstrap-tooltip
 //= require bootstrap-transition
 //= require_tree .
-
-<script>
-$(document).ready(function() {
-    $('#example').dataTable( {
-        "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>"
-    } );
-} );
-
-$.extend( $.fn.dataTableExt.oStdClasses, {
-    "sWrapper": "dataTables_wrapper form-inline"
-} );
-
-</script>
