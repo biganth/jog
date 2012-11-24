1f9772b41d30d90774246a3a8bea436126a49b1a
Anthony DeFreitas <anthony@caribbeanhotels.com>
2012-11-23 21:36:25 -0400

    Show all posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.where(:user_id => current_user)
+    @posts = Post.all #old - where(:user_id => current_user)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
