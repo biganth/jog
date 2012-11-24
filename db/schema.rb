03ddc66b43c5d163fc020a331756c46601df5886
Anthony DeFreitas <anthony@caribbeanhotels.com>
2012-11-23 23:16:26 -0400

    Add user's photo for posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108213044) do
+ActiveRecord::Schema.define(:version => 20121124030530) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20121108213044) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "user_image"
   end
 
   create_table "users", :force => true do |t|
