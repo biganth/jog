91424afbd1dc8c8055d75293f779c94ed98c8057
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-03-19 15:35:56 -0400

    Another stab at fixing omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  skip_before_filter :verify_authenticity_token
+  protect_from_forgery
   before_filter :load_application_wide_varibales
   
   def load_application_wide_varibales   
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user,repo,gist"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], 
+  {:scope => 'user, repo, gist', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
 end
