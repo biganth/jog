047505bb7eff45c116ce60c73517863ea97f1fd2
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-03-19 15:59:51 -0400

    Another try

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  def create
-    user = User.from_omniauth(env["omniauth.auth"])
-    session[:user_id] = user.id
-    redirect_to root_url, notice: "Signed in!"
-  end
   
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_url, notice: "Signed out!"
-  end
+   def create
+      @user = User.find_or_create_from_auth_hash(auth_hash)
+      self.current_user = @user
+      redirect_to root_url, notice: "Signed in!"
+    end
+    
+   protected
+
+   def auth_hash
+     request.env['omniauth.auth']
+   end
 end
