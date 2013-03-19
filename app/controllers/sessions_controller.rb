8f3d13fb1ee3e0c3925ae4a32350081f3b9bb88e
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-03-19 16:09:44 -0400

    Revert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,13 @@
 class SessionsController < ApplicationController
   
    def create
-      @user = User.find_or_create_from_auth_hash(auth_hash)
-      self.current_user = @user
-      redirect_to root_url, notice: "Signed in!"
-    end
-    
-   protected
-
-   def auth_hash
-     request.env['omniauth.auth']
+     user = User.from_omniauth(env["omniauth.auth"])
+     session[:user_id] = user.id
+     redirect_to root_url, notice: "Signed in!"
    end
-end
+
+   def destroy
+     session[:user_id] = nil
+     redirect_to root_url, notice: "Signed out!"
+   end
+ end
