15fe05d5a55b98aec0a6f41ee155727675fdf4f8
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-03-20 15:34:09 -0400

    Remove validatable

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,16 +6,3 @@ $(document).ready(function() {
     limit: 10
     });
 });
-
-
-
-jQuery("#search_field").autocomplete({
-  source: "/listings/search",
-  minLength: 2,
-  select: function(event, ui){
-    start_search=false;
-    $('search_item_id').value = ui.item.id;
-    $('search_item_type').value = ui.item.klass;
-    $('search_form').submit();
-  }
-});
