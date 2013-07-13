4a63ed74c0458b7d5539540c8b93d87693dfaf07
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-07-13 15:36:59 -0400

    Add category_id

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,5 +1,5 @@
 class SubCategory < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :category_id
   belongs_to :category
   has_many :posts
   validates :name, :category_id, :presence => true
