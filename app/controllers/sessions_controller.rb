dc3ace0dfb1e671c436f8142885657c92b742784
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-07-14 13:22:46 -0400

    More styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
    def create
      user = User.from_omniauth(env['omniauth.auth'])
      session[:user_id] = user.id
-     redirect_to root_url, notice: "Signed in!"
+     redirect_to root_url, notice: "You are signed in #{'current_user.name'}"
    end
 
    def destroy
      session[:user_id] = nil
-     redirect_to root_url, notice: "Signed out!"
+     redirect_to root_url, notice: "You signed out"
    end
  end
