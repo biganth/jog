9a255b66fa24aa85e8e998b7348a6396d579fef9
Anthony DeFreitas <anthony@caribbeanhotels.com>
2013-07-15 23:21:50 -0400

    Add dependent destroy to Sub Category

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,6 +1,6 @@
 class SubCategory < ActiveRecord::Base
   attr_accessible :name, :category_id
   belongs_to :category
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :name, :category_id, :presence => true
 end
